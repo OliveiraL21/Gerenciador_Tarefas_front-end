62bcf5a9ad463ffe1edc579357e6008c6e36b1ff
Ljoliveira <lucasoliveira508@gmail.com>
2022-11-19 15:11:10 -0300

    criando tela de clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,15 +1,25 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.scss']
+  styleUrls: ['./clientes.component.scss'],
 })
 export class ClientesComponent implements OnInit {
+  pageTitle: string = 'Clientes';
+  breadCrumbItem: string = 'Lista de Clientes';
+  form!: FormGroup;
+  isSpinning: boolean = false;
 
-  constructor() { }
+  listaClientes: any[] = [];
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      Razao_Social: [null, null],
+      Cnpj: [null, null],
+    });
   }
-
 }
